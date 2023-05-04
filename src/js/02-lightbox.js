c2cf0a20e6a9d9115c8815da10f6564faae0c58e
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-05-04 22:09:37 +0300

    Wtf(((

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,58 @@
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+galleryList.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const isImage = event.target.classList.contains('gallery__image');
+  if (!isImage) {
+    return;
+  }
+
+  const imageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+
+  const image = instance.element().querySelector('img');
+  image.onload = () => {
+    image.classList.add('opening');
+  };
+
+  document.addEventListener('keydown', onKeyPress);
+
+  function onKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', onKeyPress);
+    }
+  }
+}
+
 
 console.log(galleryItems);
