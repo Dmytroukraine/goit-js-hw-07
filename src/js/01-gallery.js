20d230c4063d0de7c58794bb2e2c6c870e2edd02
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-04 16:32:03 +0300

    WTF

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+import { galleryItems } from './src/gallery-items.js';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryMarkup = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </div>
+    `;
+    })
+    .join('');
+};
+
+galleryContainer.innerHTML = createGalleryMarkup();
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const originalSrc = event.target.closest('.gallery__link').href;
+
+  const instance = basicLightbox.create(`
+    <img src="${originalSrc}" width="800" height="600">
+`);
+
+  instance.show();
+});
 
 console.log(galleryItems);
