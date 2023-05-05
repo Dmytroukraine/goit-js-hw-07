a086969802d1d6195792049a36c2a61940de0aae
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-05 15:04:03 +0300

    start again

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,23 +1,58 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { galleryItems } from './gallery-items.js';
 
-// const galleryList = document.querySelector('.gallery');
-// const createGalleryMarkup = galleryItems
-//   .map(({ preview, original, description }) => {
-//     return `
-//       <a class="gallery__item" href="${original}">
-//         <img
-//           class="gallery__image"
-//           src="${preview}"
-//           alt="${description}"
-//         />
-//       </a>
-//     `;
-//   })
-//   .join('');
+import { galleryItems } from './gallery-items.js';
 
-// galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+const galleryList = document.querySelector('.gallery');
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join('');
 
-// new SimpleLightbox('.gallery a', {});
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+galleryList.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const isImage = event.target.classList.contains('gallery__image');
+  if (!isImage) {
+    return;
+  }
+
+  const imageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+
+  const image = instance.element().querySelector('img');
+  image.onload = () => {
+    image.classList.add('opening');
+  };
+
+  document.addEventListener('keydown', onKeyPress);
+
+  function onKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', onKeyPress);
+    }
+  }
+}
+
+
 console.log(galleryItems);
